502df04b8662d7e7739e388500e2be37622a5d0b
MilanSchuyvinck <milan_3@hotmail.nl>
2025-10-28 12:49:57 +0100

    Add ClickingScript Logic

---
+++ MosquitoGame/Assets/Scripts/ClickCounterScript.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using TMPro;
+using System.Threading;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+using Unity.VisualScripting;
+
+public class ClickCounterScript : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI TextBeaterCounter;
+    [SerializeField]
+    private int BeatingCounter = 0;
+    [SerializeField]
+    private float CooldownTime = 0.3f;
+    [SerializeField]
+    private float ResetDelay = 1;
+
+    private float cooldownTimer = 0;
+    private bool onCooldown = false;
+    private float inactivityTimer = 0f;
+    private string lastButton = "";
+
+    void Start()
+    {
+    
+    }
+
+    void Update()
+    {
+        inactivityTimer += Time.deltaTime;
+
+        if (inactivityTimer >= ResetDelay)
+        {
+            lastButton = "";
+        }
+
+        // Handle clicks
+        if (Input.GetMouseButtonDown(0))
+            HandleClick("Left");
+
+        if (Input.GetMouseButtonDown(1))
+            HandleClick("Right");
+    }
+
+
+    void HandleClick(string button)
+    {
+        inactivityTimer = 0f;
+
+        UpdateUI();
+
+        if (onCooldown)
+        {
+            if (button != lastButton)
+            {
+                onCooldown = false;
+                cooldownTimer = 0f;
+                BeatingCounter++;
+                Debug.Log("Recovered from cooldown early!");
+            }
+            else
+            {
+                Debug.Log("Still on cooldown");
+                return;
+            }
+        }
+        else
+        {
+            if (button == lastButton && lastButton != "")
+            {
+                onCooldown = true;
+                cooldownTimer = CooldownTime;
+                Debug.Log("Cooldown triggered for " + CooldownTime + "s!");
+            }
+        }
+
+        if (!onCooldown)
+        {
+            BeatingCounter++;
+        }
+
+        lastButton = button;
+    }
+
+    //void RecoverFromCooldown(string button)
+    //{
+    //    BeatingCounter++;
+    //    lastButton = button;
+    //    onCooldown = false;
+    //    Debug.Log("Recovered from cooldown");
+    //    UpdateUI();
+    //}
+
+    void UpdateUI()
+    {
+        TextBeaterCounter.text = "Beatings: " + BeatingCounter.ToString();
+    }
+}
